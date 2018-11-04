ac6f01a194577675c5192d1d4e0794311827585c
Seth Itow <sethitow@example.com>
2018-11-03 20:16:30 -0700

    Add code to clean up string values

--- imr_scraper.py
+++ imr_scraper.py
@@ -4,6 +4,7 @@ import pickle
 import hashlib
 import logging
 import json
+import re
 
 def get_content(url):
     log.info('Getting contents for URL %s', url)
@@ -26,15 +27,59 @@ def get_bullets(content):
 
 def parse_bullets(bullets, url=None):
     data = {}
+    data['Source'] = url
     bullets = bullets.split('\n')
     for item in bullets:
         if item:
             split = item.split(': ')
-            try:
+            if len(split) == 2:
                 data[split[0]] = split[1]
-            except IndexError:
-                log.exception('Unable to process bullet point: %s', item)
-    data['source'] = url
+            else:
+                log.error('Unable to process bullet point: %s', item)
+                if 'style' in item.lower():
+                    if 'flat' in item.lower():
+                        data['Style'] = 'Flat Top'
+                    elif 'button' or 'nipple' in item.lower():
+                        data['Style'] = 'Button/Nipple'
+
+    # Consolidate columns
+    for key in data:
+        if 'dimensions' in key.lower():
+            log.info('Found key: %s, renaming to "Dimensions".', key)
+            data['Dimensions'] = data.pop(key)
+        elif 'discharge' in key.lower():
+            log.info('Found key: %s, renaming to "Discharge Current".', key)
+            if type(data[key]) == type(str()):
+                raw_string = data.pop(key)
+                regex_match = re.match(r'[+-]?((\d+(\.\d*)?)|(\.\d+)) ?A', raw_string)
+                if regex_match:
+                    current = float(regex_match.group()[:-1])
+                    data['Discharge Current (A)'] = current
+                else:
+                    data['Discharge Current (A)'] = None
+        elif 'positive' in key.lower():
+            log.info('Found key: %s, renaming to "Style".', key)
+            data['Style'] = data.pop(key)
+        elif 'weight' in key.lower():
+            log.info('Found key: %s, value: %s, processing as "Weight (g)".', key, data[key])
+            if type(data[key]) == type(str()):
+                raw_string = data.pop(key)
+                regex_match = re.match(r'[+-]?((\d+(\.\d*)?)|(\.\d+)) ?g', raw_string)
+                if regex_match:
+                    weight = float(regex_match.group()[:-1])
+                    data['Weight (g)'] = weight
+                else:
+                    data['Weight (g)'] = None
+        elif key == 'Nominal Capacity':
+            log.info('Found key: %s, value: %s, processsing as "Nominal Capacity (mAh)".', key, data[key])
+            raw_string = data.pop(key)
+            regex_match = re.match(r'[+-]?((\d+(\.\d*)?)|(\.\d+)) ?mAh', raw_string)
+            capacity = float(regex_match.group()[:-3])
+            data[''.join([key, ' (mAh)'])] = capacity
+        elif 'voltage' in key.lower():
+            log.info('Found key: %s, value: %s, processsing as a voltage value.', key, data[key])
+            if type(data[key]) != type(float()):
+                data[key] = float(re.match(r'[+-]?((\d+(\.\d*)?)|(\.\d+)) ?V', data.pop(key)).group()[:-1])
     return data
 
 def get_urls(content):
